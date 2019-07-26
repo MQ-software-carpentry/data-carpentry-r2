509e737e976403299670108f5b9e17d7147ecfd8
Dr Peter Humburg <peter.humburg@mq.edu.au>
2019-07-26 14:40:25 +1000

    gather and spread

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -27,5 +27,42 @@ total_meals <- interviews %>%
   filter(total_meals > 20) %>%
   select(village, total_meals)
   
-  
-  
+interviews %>%
+  group_by(village) %>%
+  summarize(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarize(mean_no_membrs = mean(no_membrs),
+            min_membrs = min(no_membrs)) %>%
+arrange(desc(min_membrs))
+
+interviews %>% count(village, sort=TRUE)
+
+
+interviews %>%
+  group_by(village) %>%
+  summarize(
+    mean = mean(no_membrs),
+    max = max(no_membrs),
+    min = min(no_membrs),
+    n = n()
+  )
+
+
+interviews_spread <- interviews %>%
+  mutate(wall_type_logical = TRUE) %>%
+  spread(key=respondent_wall_type, value = wall_type_logical, fill = FALSE)
+
+View(interviews_spread)
+
+
+
+interviews_gather <- interviews_spread %>%
+  gather(key = "respondent_wall_type", value = "wall_type_logical",
+         burntbricks:sunbricks) %>%
+  filter(wall_type_logical) %>%
+  select(-wall_type_logical)
+
+View(interviews_gather)
